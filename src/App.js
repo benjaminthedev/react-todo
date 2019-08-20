f10f441220a8a285bd055aebd0a3f443c6d3b09b
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-08-20 15:55:13 +0100

    updated project and added UUID

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React, {Component} from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
-import AddTodo from '/.components/AddTodo';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
+
 import './App.css';
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: "Hello world",
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: "Code some react please",
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: "Time to grab a coffee",
         completed: false
       },
       {
-        id: 4,
+        id: uuid.v4(),
         title: "Check on Tesla order",
         completed: false
       }
@@ -37,10 +39,21 @@ class App extends Component {
         todo.completed = !todo.completed
       }
       return todo;
-    }) });
+    }) }); 
   }
   delToDo = (id) => {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
+  //Add To Do 
+  addTodo = (title) => {
+    //console.log(title);
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo ]})
   }
 
 
@@ -50,7 +63,7 @@ class App extends Component {
       <div className="App">
       <div className="container">
         <Header />
-        <AddTodo /> 
+        <AddTodo addTodo={this.addTodo} /> 
        <h1>To Do List - Get Stuff Done!</h1>
        <Todos todos={this.state.todos} markComplete={this.markComplete} delToDo={this.delToDo} />
       </div> 
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -6,12 +6,18 @@ export class AddTodo extends Component {
         title: ''
     }
 
-    onChange = (e) =>this.setState({ title: e.target.value });
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({ title: ''});
+    }
+
+    onChange = (e) =>this.setState({ [e.target.name]: e.target.value });
 
     render() {
         return (
             <div>
-                <form style={{ display: 'flex' }}>
+                <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
                     <input type="text" name="title" placeholder="Add A To-do" style={{ flex: '10', padding: '5px' }}  value={this.state.title} onChange={this.onChange} />
                     <input type="submit" value="submit" className="btn" style={{flex: '1'}} />
                 </form> 
